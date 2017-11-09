8b260f13ecff0763053ea12bd0150d00f21f39a7
Joel Christner <joel@maraudersoftware.com>
2017-11-08 16:30:04 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,43 @@
+﻿# StringSimilarity
+
+[![][nuget-img]][nuget]
+
+[nuget]:     https://www.nuget.org/packages/StringSimilarity.dll
+[nuget-img]: https://badge.fury.io/nu/Object.svg
+
+## Simple US-English String Similarity Score Calculator
+
+This class library calculates a score from 0 to 1 based on the similarity of two supplied strings.
+
+## Help or Feedback
+
+First things first - do you need help or have feedback?  Contact me at joel at maraudersoftware.com dot com or file an issue here!
+
+## New in v1.0.0
+
+- First release
+ 
+## Score Computation
+
+- If one string is null/empty and the other is not, the score is 0
+- If both strings are null/empty, the score is 1
+- Otherwise, a length score is multiplied by a character match score, where length score = (min / max) and character match score is (num matching / total)
+
+## Example
+
+Refer to the Test project for an example.
+```
+using Similarity;
+...
+Console.Write("String 1 : ");
+string str1 = Console.ReadLine(); 
+Console.Write("String 2 : ");
+string str2 = Console.ReadLine();
+Console.WriteLine("Score    : " + StringSimilarity.Calculate(str1, str2));
+```
+ 
+## Version History
+
+Notes from previous versions (starting with v1.0.0) will be moved here.
+v1.0.0
+- First release
---
+++ StringSimilarity/StringSimilarity.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Similarity
+{
+    public static class StringSimilarity
+    {
+        #region Public-Members
+
+        #endregion
+
+        #region Private-Members
+
+        #endregion
+
+        #region Constructors-and-Factories
+
+        #endregion
+
+        #region Public-Methods
+
+        public static decimal Calculate(string str1, string str2)
+        {
+            if (String.IsNullOrEmpty(str1) && String.IsNullOrEmpty(str2)) return 1;
+            if (String.IsNullOrEmpty(str1) && !String.IsNullOrEmpty(str2)) return 0;
+            if (!String.IsNullOrEmpty(str1) && String.IsNullOrEmpty(str2)) return 0;
+            if (str1.Equals(str2)) return 1;
+
+            string baseStr1 = BaseString(str1);
+            string baseStr2 = BaseString(str2);
+
+            decimal scoreLength = 1;
+            if (str1.Length > str2.Length) scoreLength = decimal.Divide(str2.Length, str1.Length);
+            if (str2.Length > str1.Length) scoreLength = decimal.Divide(str1.Length, str2.Length); 
+
+            char[] uniqueCharsStr1 = UniqueChars(str1);
+            char[] uniqueCharsStr2 = UniqueChars(str2);
+            int uniqueCharsLenStr1 = uniqueCharsStr1.Length;
+            int uniqueCharsLenStr2 = uniqueCharsStr2.Length;
+            int charMatch = 0;
+            int totalChars = 0;
+            decimal scoreCharMatch = 0;
+
+            if (uniqueCharsLenStr1 > uniqueCharsLenStr2)
+            {
+                totalChars = uniqueCharsLenStr1;
+                for (int i = 0; i < uniqueCharsStr1.Length; i++)
+                {
+                    if (uniqueCharsStr2.Contains(uniqueCharsStr1[i])) charMatch++;
+                }
+            }
+            else
+            {
+                totalChars = uniqueCharsLenStr2;
+                for (int i = 0; i < uniqueCharsStr2.Length; i++)
+                {
+                    if (uniqueCharsStr1.Contains(uniqueCharsStr2[i])) charMatch++;
+                }
+            }
+
+            if (totalChars > 0) scoreCharMatch = decimal.Divide(charMatch, totalChars);
+            return (decimal)(scoreLength * scoreCharMatch);
+        }
+
+        #endregion
+
+        #region Private-Methods
+
+        private static string BaseString(string str)
+        {
+            if (String.IsNullOrEmpty(str)) return "";
+
+            string ret = "";
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                int j = (int)(str[i]);
+
+                if (
+                    (j > 47 && j < 58)
+                    || (j > 64 && j < 91)
+                    || (j > 96 && j < 123)
+                    )
+                {
+                    ret += Convert.ToString(str[i]).ToLower();
+                }
+            }
+
+            return ret;
+        }
+
+        private static int CharacterCount(string str)
+        {
+            if (String.IsNullOrEmpty(str)) return 0;
+
+            int ret = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                int j = (int)(str[i]);
+
+                if (
+                    (j > 47 && j < 58)
+                    || (j > 64 && j < 91)
+                    || (j > 96 && j < 123)
+                    )
+                {
+                    ret++;
+                }
+            }
+
+            return ret;
+        }
+
+        private static char[] UniqueChars(string str)
+        {
+            if (String.IsNullOrEmpty(str)) return new List<char>().ToArray();
+            return str.Distinct().ToArray();
+        }
+
+        #endregion
+    }
+}
